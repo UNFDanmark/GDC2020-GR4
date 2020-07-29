cce643ba2dc70a3a10283e88f5a69aebc9154a9e
Christian Nix <Christian-Nix@gmx.net>
2020-07-29 22:15:17 +0200

    Added Dog plus script for random movement plus started on picking up

--- GDC2020/Assets/Scripts/Car_Script.cs
+++ GDC2020/Assets/Scripts/Car_Script.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Car_Script : MonoBehaviour
 {
-    public float car_speed = 8f;
+    public int car_speed_max = 12;
+    public int car_speed_min = 5;
 
     private int dir = 0;//0 right, 1 left
+    private int car_speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        car_speed = Random.Range(car_speed_min, car_speed_max + 1);
     }
 
     // Update is called once per frame
---
+++ GDC2020/Assets/Scripts/Dog_Script.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Dog_Script : MonoBehaviour
+{
+
+    public float dog_speed = 2;
+    public int rest_time_lower_bound = 0;
+    public int rest_time_upper_bound = 50;
+
+    public static bool held = false;
+
+    private int rest_time = 0;
+    private int rotation_value = 0;
+    private Rigidbody rigidbody;
+
+    private GameObject dog = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rest_time = Random.Range(rest_time_lower_bound, rest_time_upper_bound);
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (rest_time == 0 && !held)
+        {
+            rest_time = Random.Range(rest_time_lower_bound, rest_time_upper_bound);
+            rotation_value = Random.Range(0, 361);
+
+            transform.Rotate(0, rotation_value, 0);
+
+            //calculate the vector for movement
+            Vector3 movement = transform.forward * dog_speed;
+            //move the rigidbody
+            rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0) + movement;
+
+            rest_time = Random.Range(rest_time_lower_bound, rest_time_upper_bound);
+        }
+        else if (!held)
+        {
+            rest_time--;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float current_rotation = transform.rotation.y;
+        if (collision.gameObject.tag == "WALL")
+        {
+            if (current_rotation > 90)
+            {
+                transform.Rotate(0, transform.rotation.y - 90, 0);
+            }
+            else
+            {
+                transform.Rotate(0, transform.rotation.y + 90, 0);
+            }
+        }
+        else if (collision.gameObject.tag == "HAND")
+        {
+            dog = collision.gameObject;
+            float differnece = current_rotation - 90;
+            transform.Rotate(new Vector3(0, differnece, 0));
+            dog.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            dog.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            transform.SetParent(dog.transform);
+            dog.transform.position = transform.position;
+
+            held = true;
+        }
+    }
+}
