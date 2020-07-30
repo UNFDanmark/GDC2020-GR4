56f28ee5e4152e4d391bfe7f9b38bfd2840e5a46
Christian Nix <Christian-Nix@gmx.net>
2020-07-30 11:41:01 +0200

    Cars now respawn and get a new speed

--- GDC2020/Assets/Scripts/Car_Script.cs
+++ GDC2020/Assets/Scripts/Car_Script.cs
@@ -8,14 +8,17 @@ public class Car_Script : MonoBehaviour
     public int car_speed_max = 12;
     public int car_speed_min = 5;
 
-    //private vars for saving the direction and the speed
+    //private vars for saving the original postion and the speed
     private int car_speed;
+    private Vector3 org_position;
 
     // Start is called before the first frame update
     void Start()
     {
         //randomly determine the cars speed
         car_speed = Random.Range(car_speed_min, car_speed_max + 1);
+        //save the basic postion in a Vector3
+        org_position = transform.position;
     }
 
     // Update is called once per frame
@@ -29,5 +32,18 @@ public class Car_Script : MonoBehaviour
         //move it
         transform.Translate(car_speed * Time.deltaTime, 0, 0);
     }
-        
+
+    //when collided
+    void OnCollisionEnter(Collision collision)
+    {
+        //if we hit a wall
+       if (collision.gameObject.tag == "WALL")
+        {
+            //move car back to original position
+            transform.position = org_position;
+
+            //randomly determine a new speed for the car
+            car_speed = Random.Range(car_speed_min, car_speed_max + 1);
+        }
+    }
 }
