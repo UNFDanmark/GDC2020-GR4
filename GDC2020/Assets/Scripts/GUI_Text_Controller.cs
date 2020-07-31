1fad398baff04567c8e59526658e69ce8aae4ab2
Christian Nix <Christian-Nix@gmx.net>
2020-07-31 12:00:43 +0200

    UI with time now

---
+++ GDC2020/Assets/Scripts/GUI_Text_Controller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GUI_Text_Controller : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //var for saving time
+        float time = Time.timeSinceLevelLoad;
+        //set time on ui
+        GetComponent<Text>().text = "Time: " + time + " s";
+    }
+}
---
+++ GDC2020/Assets/Scripts/Gui_Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gui_Controller : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float timeSinceLevelLoad = Time.timeSinceLevelLoad;
+        
+    }
+}
